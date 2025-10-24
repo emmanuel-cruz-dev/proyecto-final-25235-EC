d268e9f487bb0299497f03e6c6a8852129146b9c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-24 10:19:36 -0300

    feat: adds BrowserRouter, ScrollToTop and ProgressBar to src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import NavigationBar from "./layouts/NavigationBar";
 import Footer from "./layouts/Footer";
 import AppRouter from "./routes/AppRouter";
+import ScrollToTop from "./helpers/ScrollToTop";
+import ProgressBar from "./components/ui/ProgressBar";
 
 function App() {
   return (
-    <div className="d-flex flex-column min-vh-100">
-      <NavigationBar />
-      <main className="flex-grow-1 bg-light">
-        <AppRouter />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <ProgressBar />
+      <div className="d-flex flex-column min-vh-100">
+        <NavigationBar />
+        <main className="flex-grow-1 bg-light">
+          <AppRouter />
+        </main>
+        <Footer />
+      </div>
+
+      <ScrollToTop />
+    </Router>
   );
 }
 
