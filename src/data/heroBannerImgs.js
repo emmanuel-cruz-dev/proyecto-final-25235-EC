c592fde91fb99f82acd1580578dad8393493dabe
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-31 16:12:25 -0300

    feat: adds heroBannerImgs.js to src/data/

---
+++ src/data/heroBannerImgs.js
@@ -0,0 +1,13 @@
+import HeroBanner1 from "../assets/hero-slider-1.webp";
+import HeroBanner2 from "../assets/hero-slider-2.webp";
+import HeroBanner3 from "../assets/hero-slider-3.webp";
+import HeroBanner4 from "../assets/hero-slider-4.webp";
+import HeroBanner5 from "../assets/hero-slider-5.webp";
+
+export const heroBannerImgs = [
+  HeroBanner1,
+  HeroBanner2,
+  HeroBanner3,
+  HeroBanner4,
+  HeroBanner5,
+];
