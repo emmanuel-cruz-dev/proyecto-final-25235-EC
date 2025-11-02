6d1c2e91494056f328f2e8e8247015ee57744de1
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-02 11:20:50 -0300

    feat: adds user.service.js to src/api/services/

---
+++ src/api/services/user.service.js
@@ -0,0 +1,36 @@
+import axios from "../axiosConfig";
+
+const getUsers = async () => {
+  try {
+    const response = await axios.get("/users");
+
+    if (!response.data) {
+      throw new Error("Empty response from server");
+    }
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users", error);
+    throw error;
+  }
+};
+
+const getUserById = async (id) => {
+  try {
+    const response = await axios.get(`/users/${id}`);
+
+    if (!response.data) {
+      throw new Error("Empty response from server");
+    }
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user", error);
+    throw error;
+  }
+};
+
+export const userService = {
+  getUsers,
+  getUserById,
+};
