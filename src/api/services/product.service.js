a3c450525b0b544ab9ba24fba19e4f733a793a00
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-03 10:23:47 -0300

    feat: adds getProductById to src/api/services/product.service.js

--- src/api/services/product.service.js
+++ src/api/services/product.service.js
@@ -15,6 +15,22 @@ const getProducts = async () => {
   }
 };
 
+const getProductById = async (id) => {
+  try {
+    const response = await axios.get(`/products/${id}`);
+
+    if (!response.data) {
+      throw new Error("Empty response from server");
+    }
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching product", error);
+    throw error;
+  }
+};
+
 export const productService = {
   getProducts,
+  getProductById,
 };
