1bbcb02498b6911c0656407f84b34f9f77452f31
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-04 08:39:25 -0300

    feat: adds Newsletter, ProductSlideList and FeaturesSection to src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import BannerSlider from "../components/ui/BannerSlider";
 import { heroBannerImgs } from "../data/heroBannerImgs";
+import Newsletter from "../components/ui/home/Newsletter";
+import ProductSlideList from "../components/product/ProductSlideList";
+import FeaturesSection from "../components/common/FeaturesSection";
 
 function Home() {
-  return <BannerSlider bannerImages={heroBannerImgs} />;
+  return (
+    <>
+      <BannerSlider bannerImages={heroBannerImgs} />
+      <ProductSlideList title="Productos más vendidos" />
+      <FeaturesSection />
+      <Newsletter />
+    </>
+  );
 }
 
 export default Home;
