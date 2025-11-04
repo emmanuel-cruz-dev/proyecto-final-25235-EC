e0c6574a843aab61d9c8d55433a255d03fb65a1e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-04 09:43:58 -0300

    feat: adds TestimonialSlider to src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,14 @@ import { heroBannerImgs } from "../data/heroBannerImgs";
 import Newsletter from "../components/ui/home/Newsletter";
 import ProductSlideList from "../components/product/ProductSlideList";
 import FeaturesSection from "../components/common/FeaturesSection";
+import TestimonialSlider from "../components/common/TestimonialSlider";
 
 function Home() {
   return (
     <>
       <BannerSlider bannerImages={heroBannerImgs} />
       <ProductSlideList title="Productos más vendidos" />
+      <TestimonialSlider />
       <FeaturesSection />
       <Newsletter />
     </>
