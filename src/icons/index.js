76b69042e8d65b68f73285c7022f10992b702530
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-04 07:22:59 -0300

    feat: adds index.js to src/icons/

---
+++ src/icons/index.js
@@ -0,0 +1,5 @@
+export { default as DeliveryTruck } from "./DeliveryTruck";
+export { default as HeadsetHelp } from "./HeadsetHelp";
+export { default as Gift } from "./Gift";
+export { default as Wallet } from "./Wallet";
+export { default as Undo } from "./Undo";
