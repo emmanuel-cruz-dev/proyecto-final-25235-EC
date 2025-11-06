e0ba6440b96885d686b17e14c558056ad4676a36
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-06 12:21:51 -0300

    feat: updates structure and functions in src/routes/PrivateRoute.jsx

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
 function PrivateRoute({ children }) {
-  const { isAuthenticated } = useAuth();
+  const { user } = useAuth();
+  const location = useLocation();
 
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
 }
 
 export default PrivateRoute;
