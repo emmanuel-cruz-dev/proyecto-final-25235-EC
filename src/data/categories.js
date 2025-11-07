cbdd3851d80c01d9fecb6ca6e70daf6cafdc6588
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-07 07:27:50 -0300

    feat: adds categories.js to src/data/

---
+++ src/data/categories.js
@@ -0,0 +1,7 @@
+export const categories = [
+  { id: "all", name: "Todos los productos" },
+  { id: "technology", name: "Tecnología" },
+  { id: "gaming", name: "Gaming" },
+  { id: "men's clothing", name: "Ropa de Hombre" },
+  { id: "women's clothing", name: "Ropa de Mujer" },
+];
