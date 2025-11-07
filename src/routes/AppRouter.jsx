c77ae94adfbd929fa6b6a8294b55c2caca043862
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-07 07:46:57 -0300

    feat: updates structure in src/routes/AppRouter.jsx

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,44 +1,31 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import { publicRoutes, privateRoutes, fallbackRoute } from "./config";
 import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoute";
-
-const Home = lazy(() => import("../pages/Home"));
-const NotFound = lazy(() => import("../pages/NotFound"));
-const Login = lazy(() => import("../pages/Login"));
-const Register = lazy(() => import("../pages/Register"));
-const Profile = lazy(() => import("../pages/Profile"));
+import Loader from "../components/common/Loader";
 
 function AppRouter() {
   return (
-    <Suspense fallback={<div>Cargando...</div>}>
+    <Suspense fallback={<Loader />}>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/login"
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <PublicRoute>
-              <Register />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <PrivateRoute>
-              <Profile />
-            </PrivateRoute>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
+        {publicRoutes.map(({ path, element, isPublic }) => (
+          <Route
+            key={path}
+            path={path}
+            element={isPublic ? <PublicRoute>{element}</PublicRoute> : element}
+          />
+        ))}
+
+        {privateRoutes.map(({ path, element }) => (
+          <Route
+            key={path}
+            path={path}
+            element={<PrivateRoute>{element}</PrivateRoute>}
+          />
+        ))}
+
+        <Route path={fallbackRoute.path} element={fallbackRoute.element} />
       </Routes>
     </Suspense>
   );
