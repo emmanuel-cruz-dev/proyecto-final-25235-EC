524d3a00a6b937110fcd8ac0c3b66611efdc4c6c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-07 08:32:54 -0300

    feat: adds ProductsGallery.jsx to src/components/product/

---
+++ src/components/product/ProductsGallery.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProductCard from "./ProductCard";
+import ErrorMessage from "../common/ErrorMessage";
+import { handleRetry } from "../../utils/utils";
+
+function ProductsGallery({ title, products, loading, error }) {
+  return (
+    <Container>
+      <header>
+        <h1 className="display-6 text-center fw-bold my-4">{title}</h1>
+      </header>
+      <Row className="g-3 align-items-center justify-content-center">
+        {loading ? (
+          Array.from({ length: 6 }).map((_, index) => (
+            <Col key={`placeholder-${index}`} xs={12} md={6} lg={4} xl={4}>
+              <ProductCard isLoading={true} />
+            </Col>
+          ))
+        ) : error ? (
+          <ErrorMessage
+            error={error}
+            entity="Productos"
+            onRetry={handleRetry}
+          />
+        ) : (
+          products.map((product) => (
+            <Col key={product.id} xs={12} md={6} lg={4} xl={4}>
+              <ProductCard
+                id={product.id}
+                title={product.title}
+                price={product.price}
+                description={product.description}
+                category={product.category}
+                image={product.image}
+              />
+            </Col>
+          ))
+        )}
+      </Row>
+    </Container>
+  );
+}
+
+export default ProductsGallery;
