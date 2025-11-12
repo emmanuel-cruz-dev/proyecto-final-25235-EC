428605963cdb47942225104d506b0d1615cf2623
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-12 07:26:23 -0300

    feat: adds Loader.jsx to src/routes/PrivateRoute.jsx

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import Loader from "../components/common/Loader";
 
 function PrivateRoute({ children }) {
-  const { user } = useAuth();
+  const { user, loading, authLoading } = useAuth();
   const location = useLocation();
+
+  if (loading || authLoading) {
+    return <Loader />;
+  }
 
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
