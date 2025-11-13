4b92e8ec24bc5b7d7cd2c66373aa4263b7b6c814
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-13 09:03:28 -0300

    feat: adds BannerCarousel to src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import BannerSlider from "../components/ui/BannerSlider";
 import { heroBannerImgs } from "../data/heroBannerImgs";
 import Newsletter from "../components/ui/home/Newsletter";
 import ProductSlideList from "../components/product/ProductSlideList";
 import FeaturesSection from "../components/common/FeaturesSection";
 import TestimonialSlider from "../components/common/TestimonialSlider";
+import BannerCarousel from "../components/ui/BannerCarousel";
 
 function Home() {
   return (
     <>
-      <BannerSlider bannerImages={heroBannerImgs} />
+      <BannerCarousel images={heroBannerImgs} />
       <ProductSlideList title="Productos más vendidos" />
       <TestimonialSlider />
       <FeaturesSection />
