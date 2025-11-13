78478e77cbbd90db70d5029951c43676e8c9c418
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-13 13:07:26 -0300

    feat: updates components props in src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,20 @@ import ProductSlideList from "../components/product/ProductSlideList";
 import FeaturesSection from "../components/common/FeaturesSection";
 import TestimonialSlider from "../components/common/TestimonialSlider";
 import BannerCarousel from "../components/ui/BannerCarousel";
+import { useProducts } from "../hooks/useProducts";
 
 function Home() {
+  const { products, loading, error } = useProducts(1, 6);
+
   return (
     <>
       <BannerCarousel images={heroBannerImgs} />
-      <ProductSlideList title="Productos más vendidos" />
+      <ProductSlideList
+        title="Productos Destacados"
+        products={products}
+        loading={loading}
+        error={error}
+      />
       <TestimonialSlider />
       <FeaturesSection />
       <Newsletter />
