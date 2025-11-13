a1725bd9cfdf3daa430f4dc3883bc7330f41f09e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-13 09:02:33 -0300

    feat: adds BannerCarousel to src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootswatch/dist/cerulean/bootstrap.css";
+//import "bootswatch/dist/materia/bootstrap.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
