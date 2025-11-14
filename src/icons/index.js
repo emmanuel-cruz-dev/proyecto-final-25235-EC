82af68e35b055c2b281f7ae62ad06fb8ae4ac8bd
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-14 09:00:13 -0300

    feat: adds new classes to src/index.css

--- src/icons/index.js
+++
@@ -1,5 +0,0 @@
-export { default as DeliveryTruck } from "./DeliveryTruck";
-export { default as HeadsetHelp } from "./HeadsetHelp";
-export { default as Gift } from "./Gift";
-export { default as Wallet } from "./Wallet";
-export { default as Undo } from "./Undo";
