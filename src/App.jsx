bdf2582902cedd52dcedce5af02c1a1ac0b57a9b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-15 11:31:05 -0300

    feat: adds imports React to App.jsx, NotFound.jsx and Product.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavigationBar from "./layouts/NavigationBar";
 import Footer from "./layouts/Footer";
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useParams } from "react-router-dom";
 import { useProductById, useProductsByCategory } from "../hooks/useProducts";
 import ProductSlideList from "../components/product/ProductSlideList";
